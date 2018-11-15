aaf7bc711e96858edb96b06fd798b552262ec9ad
David Ellis <isv.damocles@gmail.com>
2018-11-15 11:03:36 -0800

    First method mostly working

--- h3napi.c
+++ h3napi.c
@@ -1,35 +1,54 @@
 #include <node_api.h>
 #include <stdio.h>
+#include "h3api.h"
 
-napi_value print (napi_env env, napi_callback_info info) {
-  napi_value argv[1];
-  size_t argc = 1;
+napi_value napiGeoToH3(napi_env env, napi_callback_info info) {
+  napi_value argv[3];
+  size_t argc = 3;
 
   napi_get_cb_info(env, info, &argc, argv, NULL, NULL);
 
-  if (argc < 1) {
+  if (argc < 3) {
     napi_throw_error(env, "EINVAL", "Too few arguments");
     return NULL;
   }
 
-  char str[1024];
-  size_t str_len;
+  double lat;
+  double lng;
+  int res;
 
-  if (napi_get_value_string_utf8(env, argv[0], (char *) &str, 1024, &str_len) != napi_ok) {
-    napi_throw_error(env, "EINVAL", "Expected string");
+  if (napi_get_value_double(env, argv[0], (double *) &lat) != napi_ok) {
+    napi_throw_error(env, "EINVAL", "Expected numeric latitude");
     return NULL;
   }
 
-  printf("Printed from C: %s\n", str);
+  if (napi_get_value_double(env, argv[1], (double *) &lng) != napi_ok) {
+    napi_throw_error(env, "EINVAL", "Expected numeric longitude");
+    return NULL;
+  }
 
-  return NULL;
+  if (napi_get_value_int32(env, argv[2], (int *) &res) != napi_ok) {
+    napi_throw_error(env, "EINVAL", "Expected numeric resolution");
+    return NULL;
+  }
+
+  GeoCoord geo = { lat, lng };
+  H3Index h3 = geoToH3(&geo, res);
+  char h3String[17];
+  h3ToString(h3, h3String, 17);
+  napi_value result;
+  if (napi_create_string_utf8(env, h3String, 17, &result) != napi_ok) {
+    napi_throw_error(env, "EINVAL", "Could not write H3 string");
+  }
+
+  return result;
 }
 
 
 napi_value init_all (napi_env env, napi_value exports) {
-  napi_value print_fn;
-  napi_create_function(env, NULL, 0, print, NULL, &print_fn);
-  napi_set_named_property(env, exports, "print", print_fn);
+  napi_value geoToH3Fn;
+  napi_create_function(env, NULL, 0, napiGeoToH3, NULL, &geoToH3Fn);
+  napi_set_named_property(env, exports, "geoToH3", geoToH3Fn);
   return exports;
 }
 
