e0f9674b78c3925427453feef57c0767ceb8fcef
David Ellis <isv.damocles@gmail.com>
2019-02-18 20:16:45 -0800

    Move the prebuild command to a shell script to escape escaping

---
+++ .prebuild.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+set -vex;
+npm run clean &&
+  git clone https://github.com/uber/h3 &&
+  cd h3 &&
+  git checkout v3.4.0 &&
+  if command -v make; then
+    cmake . -DCMAKE_C_FLAGS=-fPIC && make;
+  else
+    cmake . -DCMAKE_VS_PLATFORM_NAME=${Platform} `if [[ "${Platform}" == "x64" ]]; then echo "-G \"Visual Studio 14 Win64\""; fi` && msbuild.exe h3.vcxproj;
+  fi &&
+  cd .. &&
+  prebuildify --target=node@`node --version | sed s/v//`
+
